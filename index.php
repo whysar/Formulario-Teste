fcfa33000777b7388b42ffc8fa4845db75c0b824
gabrieladelarioop <gabrieladelarioop@gmail.com>
2019-10-14 13:47:34 -0300

    problem correction

--- index.php
+++ index.php
@@ -1,50 +1,80 @@
 <html>
+
 <head>
+
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
 </head>
-    <body>
+
+<body> 
+    <body style = "font-family:times, times new roman, serif;" bgcolor="#C0C0C0" text="#008000" >
+
     <form name="form" action="/script.php" method="post">
+
         <table>
         <tr>
-        <th colspan="2">Feedback</th>
+        <th colspan="2">Feedback
+        </th>
         </tr>
         <tr>
-        <td><label>Name*:</label></td>
-        <td><input type="text" name="name" id="name" size="40" maxlength="30" required></td>
-        </tr>
-
-        <tr>
-        <td><label>E-mail:</label></td>
-        <td><input type="text" name="email" id="email" size="40" maxlength="30"></td>
-        </tr>
-
-        <tr>
-        <td><label>Telephone:</label></td>
-        <td><input type="text" name="ddd" id="ddd" size="2" maxlength="2">
-        <input type="text" name="number" id="number" size="9" maxlength="9"></td>
+        <td> 
+        <label> Name*:</label> 
+        </td>
+        <td> 
+        <input type="text" name="name" id="name" size="40" maxlength="30" required > 
+        </td>
         </tr>
 
         <tr>
-        <td><label>Feedback*:</label></td>
-        <td><textarea name="feedback" id="feedback" cols="40" rows="5" required> </textarea>
+        <td> 
+        <label> E-mail:</label> 
+        </td>
+        <td> 
+        <input type="text" name="email" id="email" size="40" maxlength="30" > 
+        </td>
+        </tr>
+
+        <tr>
+        <td> 
+        <label> Telephone:</label>
+        </td>
+        <td> 
+        <input type="text" name="ddd" id="ddd" size="2" maxlength="2" >
+        <input type="text" name="number" id="number" size="9" maxlength="9" > 
+        </td>
+        </tr>
+
+        <tr>
+        <td> 
+        <label> Feedback*: </label> 
+        </td>
+        <td> 
+        <textarea name="feedback" id="feedback" cols="40" rows="5" required > </textarea>
         </tr>
 
 
         <tr>
-        <td><label>Service Quality:</label></td>
-        <td><input type="radio" name="quality" id="quality" value="terrible"> Terrible
-        <input type="radio" name="quality" id="quality" value="bad"> Bad
-        <input type="radio" name="quality" id="quality" value="reasonable"> Reasonable
-        <input type="radio" name="quality" id="quality" value="good"> Good
-        <input type="radio" name="quality" id="quality" value="great"> Great</td>
+        <td> 
+        <label> Service Quality: 
+        </label> 
+        </td>
+        <td> 
+        <input type="radio" name="quality" id="quality" value="terrible" > Terrible
+        <input type="radio" name="quality" id="quality" value="bad" > Bad
+        <input type="radio" name="quality" id="quality" value="reasonable" > Reasonable
+        <input type="radio" name="quality" id="quality" value="good" > Good
+        <input type="radio" name="quality" id="quality" value="great" > Great 
+        </td>
         </tr>
 
-		<tr><td>
-        <input type="submit" name="Register" id="Register" value="Submit" /> </td>
+		<tr>
+        <td>
+        <input type="submit" name="Register" id="Register" value="Submit" > 
+        </td>
 		</tr>
 
     </form>
 
 </body>
-<body style = "font-family:times, times new roman, serif;" bgcolor="#C0C0C0" text="#008000" >
+
 </html>
--- script.php
+++ script.php
@@ -1,19 +1,41 @@
 
 <html>
+
 <body>
+
+  <body style = "font-family:times, times new roman, serif;" bgcolor="#C0C0C0" text="#008000" >
+
 <?php
-if (empty($_POST["name"])){
+
+if (empty($_POST["name"])) {
+
   echo "<br>Name field needs to be filled in!";
+
 }
-  elseif (empty($_POST["feedback"])){
+  elseif (empty($_POST["feedback"])) {
+
 echo "<br>Feedback field needs to be filled in!";
+
 }
+
 else {
+
 ?>
+
+<br>
+
 Welcome <?php echo $_POST["name"]; ?><br>
+
+<br>
+
 We appreciate your feedback.<br>
+
 <?php
+
 }
+
 ?>
+
 </body>
+
 </html>
