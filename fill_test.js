46d109bc1bea6f004139b5e79277e0af6195bbf4
Samantha <53868840+whysar@users.noreply.github.com>
2019-10-17 13:14:42 -0300

    Add files via upload

---
+++ fill_test.js
@@ -0,0 +1,16 @@
+describe('Fill Test', function() {
+    it('Visits the form', function() {
+      cy.visit('http://localhost:8000/')
+    })
+  
+  it('Submit values', function() {
+    cy.get('#name').type('Teste')
+    cy.get('#email').type('example@example.com')
+    cy.get('#ddd').type('31')
+    cy.get('#number').type('000000000')
+    cy.get('#feedback').type('abcdefghijklmnopqrstuvwxyz')
+    cy.get('[type="radio"]').check('reasonable')
+    cy.get('input[name="Register"]').click()
+
+  })
+})
